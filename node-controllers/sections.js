ef79a26e080ee37c1b9a58cce8443f5d28f8fc98
Daniel Kats <boompigdev@gmail.com>
2014-09-09 11:02:55 -0400

    used pg_cruft for sections

--- node-controllers/sections.js
+++ node-controllers/sections.js
@@ -1,74 +1,53 @@
 var pg = require("pg");
+var cruft = require("./pg_cruft.js");
 
 /*
  * conString is the postgres connection string
  */
 exports.getSections = function (request, response, next, conString) {
+    "use strict";
+    var query, data;
     if (request.query.username) {
+        query = "SELECT * FROM sections WHERE username=$1";
+        data = [request.query.username];
         console.log("Fetching all sections for user %s", request.query.username);
-        // fetch all the sections from the DB
-        pg.connect(conString, function (err, client, done) {
-            if (err) {
-                return console.error("failed connecting to PG server.");
-            }
-            client.query("SELECT * FROM sections WHERE username=$1", [request.query.username], function(err, result) {
-                done();
-                if (err) {
-                    console.error("Error running query", err);
-                }
-                console.log("Wrote %d rows out", result.rows.length);
-                response.send(result.rows);
-                response.end();
-            });
-        });
     } else {
-        // fetch all the sections from the DB
-        pg.connect(conString, function (err, client, done) {
-            if (err) {
-                return console.error("failed connecting to PG server.");
-            }
-            client.query("SELECT * FROM sections", function(err, result) {
-                done();
-                if (err) {
-                    console.error("Error running query", err);
-                }
-                console.log("Wrote %d rows out", result.rows.length);
-                response.send(result.rows);
-                response.end();
-            });
-        });
+        query = "SELECT * FROM sections";
+        data = [];
     }
+    cruft.query(query, data, conString, function (err, result) {
+        if (err) {
+            return response.send({ status: "error", msg: err.toString() });
+        } else {
+            console.log("Wrote %d rows out", result.rows.length);
+            response.send(result.rows);
+        }
+    });
 };
 
 exports.deleteSection = function (request, response, next, conString) {
-    // make sure the name is provided
+    "use strict";
+    // make sure the id is provided
     if (! request.params.id) {
         response.send({status: "error", msg: "No section id provided"});
-        console.error("Section ID not provided in request");
-        return;
+        return console.error("Section ID not provided in request");
     }
 
-    pg.connect(conString, function (err, client, done) {
+    var query = "DELETE FROM sections WHERE id=$1";
+    var data = [request.params.id];
+    cruft.query(query, data, conString, function (err, result) {
         if (err) {
-            return console.error("failed connecting to PG server.");
+            return response.send({ status: "error", msg: err.toString() });
+        } else {
+            console.log("Deleted %d rows", result.rowCount);
+            response.send({ status: "success" });
         }
-        client.query(
-            "DELETE FROM sections WHERE id=$1",
-            [request.params.id],
-            function(err, result) {
-                done();
-                if (err) {
-                    console.error("Error running query", err);
-                }
-                console.log("Deleted %d rows", result.rowCount);
-                response.send({status: "success"});
-                response.end();
-            }
-        );
     });
 };
 
 exports.addSection = function(request, response, next, conString) {
+    "use strict";
+    // name must be specified for section
     if (!request.body.name) {
         response.send({status: "error", msg: "Empty section name provided"});
         return console.error("Empty section name");
@@ -77,26 +56,14 @@ exports.addSection = function(request, response, next, conString) {
         return console.error("username for section not provided");
     }
 
-    pg.connect(conString, function (err, client, done) {
+    var query = "INSERT INTO sections (name, section_number, username) VALUES ($1, $2, $3) RETURNING id";
+    var data = [request.body.name, request.body.section_number, request.body.username];
+    cruft.query(query, data, conString, function (err, result) {
         if (err) {
-            response.send({status: "error", msg: "failed to connect to PG server"});
-            return console.error("failed connecting to PG server.");
+            return response.send({ status: "error", msg: err.toString() });
+        } else {
+            console.log("Inserted with ID", result.rows[0].id);
+            response.send({ status: "success", "insert_id": result.rows[0].id });
         }
-
-        client.query("INSERT INTO sections (name, section_number, username) VALUES ($1, $2, $3)", [request.body.name, request.body.section_number, request.body.username], function (err, result) {
-            done();
-            if (err) {
-                console.error("Failed running query", err);
-                response.send({status: "error", msg: err});
-            } else {
-                console.log("Inserted 1 row, getting insert ID...");
-
-                // get the insert ID
-                client.query("SELECT id FROM sections WHERE name=$1 and section_number=$2 and username=$3", [request.body.name, request.body.section_number, request.body.username], function (err, result) {
-                    console.log("Insert ID is " + result.rows[0].id);
-                    response.send({status: "success", insert_id: result.rows[0].id});
-                });
-            }
-        });
     });
 };
